25d66c34c69b8dd0c897dacea4521cf6125eb569
Issue <zhushisong@skio.com>
2017-11-14 17:20:10 +0800

    fix

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        <h1>Hello Word</h1>
+    }
+
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+//__dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录
+module.exports = {
+    entry: ['webpack/hot/dev-server', __dirname + "/index.js"],
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/, // test 匹配js和jsx）
+                exclude: /node_modules/, //屏蔽不需要处理的文件
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin() //热模块替换插件
+    ],
+    devServer: {
+        contentBase: './build',
+        // colors: true,
+        historyApiFallback: true,
+        inline: true,
+        port: 8888,
+    // process: true,
+    }
+};
