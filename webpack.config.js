d3e3886e97d494088e146c6307c5232d5e6dc7b5
Issue <zhushisong@skio.com>
2017-11-15 10:29:26 +0800

    bundle folder move to assets

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: __dirname + "/build",
         filename: "bundle.js",
-        publicPath: "/build"
+        publicPath: "/assets/"
     },
     module: {
         loaders: [
